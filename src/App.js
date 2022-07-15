0fa1bdb3b2c89809b7d174d503fe9d78ea451666
unknown <v3rdugo@yahoo.com>
2022-07-15 15:57:57 -0700

    fix bug and align footer correctly

--- src/App.js
+++ src/App.js
@@ -34,12 +34,20 @@ const App = () => {
                     </Switch>
                 </div>
             </Layout>
+        
+
+            <div className="footer">
+                <Typography.Title level={5} style={{ color: 'white', textAlign: 'center '}}>
+                    Crypto Tracker <br />
+                    All rights reserved
+                </Typography.Title>
+                <Space>
+                    <Link to="/">Home</Link>
+                    <Link to="/exchanges">Exchanges</Link>
+                    <Link to="/news">News</Link>
+                </Space>
+            </div>
         </div>
-
-        <div className="footer">
-
-        </div>
-
     </div>
   )
 }
