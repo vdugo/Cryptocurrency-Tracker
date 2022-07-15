00e89f4565c836c60391f4547749d8ff6e56bd45
unknown <v3rdugo@yahoo.com>
2022-07-15 15:33:46 -0700

    update components/index.js to export components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,8 @@
 import Navbar from './Navbar'
+import Cryptocurrencies from './Cryptocurrencies'
+import CryptoDetails from './CryptoDetails'
+import Exchanges from './Exchanges'
+import Homepage from './Homepage'
+import News from './News'
 
-export { Navbar }
+export { Navbar, Cryptocurrencies, CryptoDetails, Exchanges, Homepage, News }
