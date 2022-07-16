2c7301a19dc618fffd878eb344c2b2d3d2e45505
unknown <v3rdugo@yahoo.com>
2022-07-16 16:52:44 -0700

    add show more page to show top 100 cryptocurrencies, fix null cryptos list bug

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,25 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import millify from 'millify'
 import { Link } from 'react-router-dom'
 import { Card, Row, Col, Input } from 'antd'
 
 import { useGetCryptosQuery } from '../services/cryptoApi'
 
-const Cryptocurrencies = () => {
-  const { data: cryptosList, isFetching } = useGetCryptosQuery()
-  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins)
+const Cryptocurrencies = ( { simplified } ) => {
+  const count = simplified ? 10 : 100
+  const { data, isFetching } = useGetCryptosQuery(count)
+  const [cryptos, setCryptos] = useState()
 
-  console.log(cryptos)
+  useEffect(() => {
+    setCryptos(data?.data?.coins)
+  }, [data])
+
+  if (isFetching) return 'Loading...'
 
   return (
     <>
-      <Row gutters={[32,32]} className="crypto-card-container">
-        {cryptos.map((crypto) => (
+      <Row gutter={[32, 32]} className="crypto-card-container">
+        {cryptos?.map((crypto) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={crypto.uuid}>
             <Link to={`/crypto/${crypto.uuid}`}>
               <Card title={`${crypto.rank}. ${crypto.name}`}
                extra={<img className="crypto-image" src={crypto.iconUrl}
-               hoverable alt={`${crypto.symbol}`}/>}>
+               hoverable="true" alt={`${crypto.symbol}`}/>}>
 
                 <p>Price: ${millify(crypto.price)}</p>
                 <p>Market Cap: ${millify(crypto.marketCap)}</p>
