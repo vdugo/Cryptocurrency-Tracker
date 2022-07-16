09de2e9b35e2d1011cbd7474091710472f158c0c
unknown <v3rdugo@yahoo.com>
2022-07-16 14:38:00 -0700

    Set up RapidAPI API, add environment variables, set up API data for Homepage get all cryptos data

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
+    'X-RapidAPI-Host': process.env.REACT_APP_RAPIDAPI_HOST
+}
+
+const baseUrl = `https://${process.env.REACT_APP_RAPIDAPI_HOST}`
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/coins')
+        })
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi
