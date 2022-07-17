d6710a812b4aeedfc37d01c7207e5afd19326c51
unknown <v3rdugo@yahoo.com>
2022-07-16 17:11:20 -0700

    don't show search bar on homepage with only 10 cryptos

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -22,9 +22,11 @@ const Cryptocurrencies = ( { simplified } ) => {
 
   return (
     <>
-      <div className="search-crypto">
-        <Input placeholder="Search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value)} />
-      </div>
+      {!simplified && (
+          <div className="search-crypto">
+            <Input placeholder="Search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value)} />
+          </div>
+      )}
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((crypto) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={crypto.uuid}>
