9092484343521a1d84b8bf67f25d2924e3c4ce06
unknown <v3rdugo@yahoo.com>
2022-07-17 13:56:45 -0700

    set up select time period options for cryptoDetails component

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -14,7 +14,11 @@ const CryptoDetails = () => {
   const { coinId } = useParams()
   const [timePeriod, setTimePeriod] = useState('7d')
   const { data, isFetching } = useGetCryptoDetailsQuery(coinId)
+
+  if (isFetching) return 'Loading...'
+
   const cryptoDetails = data?.data?.coin
+  console.log(data)
 
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
 
@@ -35,9 +39,25 @@ const CryptoDetails = () => {
   ];
 
   return (
-    <div>
-      CryptoDetails {coinId}
-    </div>
+    <Col className="coin-detail-container">
+      <Col className="coin-heading-container">
+        <Title level={2} className="coin-name">
+          {cryptoDetails.name} {cryptoDetails.symbol} Price
+        </Title>
+        <p>
+          {cryptoDetails.name} live price in US dollars.
+          View value statistics, market cap and supply.
+        </p>
+      </Col>
+      <Select 
+      defaultValue="7d" 
+      className="select-timeperiod" 
+      placeholder="Select Time Period"
+      onChange={(value) => setTimePeriod(value)}
+      >
+        {time.map((date) => (<Option key={date}>{date}</Option>))}
+      </Select>
+    </Col>
   )
 }
 
