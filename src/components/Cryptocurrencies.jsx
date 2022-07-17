d6a4d8d394222f5ac622cbd657733cd16f97ccae
unknown <v3rdugo@yahoo.com>
2022-07-16 17:05:19 -0700

    add search functionality to filter cryptos

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -7,17 +7,24 @@ import { useGetCryptosQuery } from '../services/cryptoApi'
 
 const Cryptocurrencies = ( { simplified } ) => {
   const count = simplified ? 10 : 100
-  const { data, isFetching } = useGetCryptosQuery(count)
-  const [cryptos, setCryptos] = useState()
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count)
+  const [cryptos, setCryptos] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
 
   useEffect(() => {
-    setCryptos(data?.data?.coins)
-  }, [data])
+    setCryptos(cryptosList?.data?.coins)
+
+    const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    setCryptos(filteredData)
+  }, [cryptosList, searchTerm])
 
   if (isFetching) return 'Loading...'
 
   return (
     <>
+      <div className="search-crypto">
+        <Input placeholder="Search Cryptocurrency" onChange={(e) => setSearchTerm(e.target.value)} />
+      </div>
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((crypto) => (
           <Col xs={24} sm={12} lg={6} className="crypto-card" key={crypto.uuid}>
