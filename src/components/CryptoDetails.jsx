d3f9868f66105b3838195dcc19a691d0f13e4fc7
unknown <v3rdugo@yahoo.com>
2022-07-17 15:48:59 -0700

    don't console.log() the data

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -20,8 +20,6 @@ const CryptoDetails = () => {
 
   if (isFetching) return 'Loading...'
 
-  console.log(coinHistory)
-
   const cryptoDetails = data?.data?.coin
 
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y']
